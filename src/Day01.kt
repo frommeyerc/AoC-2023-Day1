c6b69e2ef4d7bd1476bdef239195b1ec7e864752
Christian Frommeyer <christian.frommeyer@personio.de>
2023-12-01 09:10:39 +0100

    Day01 part 2 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,5 @@
+val DIGIT_WORDS = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.map {
@@ -11,13 +13,34 @@ fun main() {
         }.sum()
     }
 
+    fun wordToDigit(s: String): Int {
+        return DIGIT_WORDS.indexOf(s) + 1
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            var firstDigit = it.indexOfFirst { c -> c.isDigit() }
+            if (firstDigit == -1) firstDigit = Int.MAX_VALUE
+            val firstWord = it.findAnyOf(DIGIT_WORDS)
+            firstDigit = if (firstWord != null && firstDigit > firstWord.first) {
+                wordToDigit(firstWord.second)
+            } else
+                it[firstDigit].digitToInt()
+            var secondDigit = it.indexOfLast { it.isDigit() }
+            val lastWord = it.findLastAnyOf(DIGIT_WORDS)
+            secondDigit = if (lastWord != null && secondDigit < lastWord.first) {
+                wordToDigit(lastWord.second)
+            } else
+                it[secondDigit].digitToInt()
+            (10 * firstDigit + secondDigit)
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    val testInput1 = readInput("Day01_test")
+    check(part1(testInput1) == 142)
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
