da4b83e9db82cf0efd8ff90c74011d7c774dab89
Christian Frommeyer <christian.frommeyer@personio.de>
2023-12-01 07:47:12 +0100

    Day01 part 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,14 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map {
+            val digits = it.filter { c -> c.isDigit() }
+            when (digits.length) {
+                0 -> 0
+                1 -> (digits + digits).toInt()
+                2 -> digits.toInt()
+                else -> digits.removeRange(1, digits.length - 1).toInt()
+            }
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +17,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
