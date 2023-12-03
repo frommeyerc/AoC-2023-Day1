f67472f5bd9cf581edc685e9ffb99711e9fc5c8c
Christian Frommeyer <christian.frommeyer@personio.de>
2023-12-03 23:06:03 +0100

    Day02 part 1 done

---
+++ src/Day02.kt
@@ -0,0 +1,53 @@
+fun main() {
+    fun checkPossible(game: Game): Boolean {
+        game.draws.forEach {
+            if (it.blue > 14 || it.green > 13 || it.red > 12) return false
+        }
+        return true
+    }
+
+    fun part1(input: List<String>): Int {
+        val regex = """Game ([0-9]+): |([0-9]+) blue|([0-9]+) green|([0-9]+) red|(;)""".toRegex()
+        return input.map {
+            var pos = 0
+            var match = regex.find(it, pos)
+            val game = Game(match!!.groupValues[1].toInt())
+            while (pos < it.length) {
+                pos = match!!.range.last + 1
+                val draw = Draw()
+                game.draws.add(draw)
+                match = regex.find(it, pos)
+                while (pos < it.length && match!!.groups[5] == null) {
+                    if (match.groups[2] != null) draw.blue = draw.blue + match.groupValues[2].toInt()
+                    if (match.groups[3] != null) draw.green = draw.green + match.groupValues[3].toInt()
+                    if (match.groups[4] != null) draw.red = draw.red + match.groupValues[4].toInt()
+                    pos = match.range.last + 1
+                    match = regex.find(it, pos)
+                }
+            }
+            game
+        }.sumOf { if (checkPossible(it)) it.number else 0}
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 8)
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
+
+class Game(val number: Int) {
+    val draws: MutableList<Draw> = mutableListOf()
+}
+
+class Draw() {
+    var green: Int = 0;
+    var blue: Int = 0;
+    var red: Int = 0;
+}
