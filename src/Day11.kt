3b34f27d627c20aab793790901603dc38b9bb1cc
Christian Frommeyer <christian.frommeyer@personio.de>
2023-12-11 08:08:11 +0100

    Day11 part 2 done

--- src/Day11.kt
+++ src/Day11.kt
@@ -31,8 +31,32 @@ fun main() {
         return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val emptyCols = mutableListOf<Int>()
+        for (i in input[0].indices) {
+            var j = 0
+            while (j < input.size && input[j][i] == '.') j++
+            if ( j == input.size) emptyCols.add(i)
+        }
+        var count = 0
+        var y = 0L
+        val galaxies = input.map {
+            val galaxies = mutableListOf<Triple<Long, Long, Int>>()
+            var x = 0L
+            for (i in it.indices) {
+                if (it[i] == '#') galaxies.add(Triple(x, y, count++))
+                x += if (emptyCols.contains(i)) 1_000_000L else 1L
+            }
+            y += if (galaxies.isEmpty()) 1_000_000L else 1L
+            galaxies
+        }.flatten()
+        var sum = 0L
+        for (i in galaxies.indices) {
+            for (j in (i+1)..<galaxies.size) {
+                sum += abs(galaxies[i].first - galaxies[j].first) + abs(galaxies[i].second - galaxies[j].second)
+            }
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
