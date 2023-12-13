e87c0dddda4abdc5b80e5b9a5d89e46733994eca
Christian Frommeyer <christian.frommeyer@personio.de>
2023-12-13 07:05:46 +0100

    Day12 part 2 done.

--- src/Day12.kt
+++ src/Day12.kt
@@ -41,10 +41,15 @@ fun main() {
     var count = 0
     fun createOptions(base: List<String>, freeOperational: Int, pattern: String): Int {
         noSolutions = mutableSetOf()
-        println("Pattern $count")
         count++
-        return fillBucket(base, pattern, Array(base.size + 1) {0}, freeOperational, 0)
-            .count()
+        val start = System.currentTimeMillis()
+        try {
+            return fillBucket(base, pattern, Array(base.size + 1) { 0 }, freeOperational, 0)
+                .count()
+        } finally {
+            val time = System.currentTimeMillis() - start;
+            println("Pattern $count:  $time ms")
+        }
     }
 
     fun part1(input: List<String>): Int {
@@ -72,11 +77,46 @@ fun main() {
         }
     }
 
+    fun arrangements(spread: String, compact: List<Int>, cache: MutableMap<String, Long> = hashMapOf()): Long {
+        var count = 0L
+        for (i in spread.indices) {
+            if (i > 0 && spread[i - 1] == '#') return count
+            val char = spread[i]
+            if (char == '.') continue
+            if (compact.isEmpty() && char == '#') return 0
+            if (compact.isEmpty()) continue
+            val matchSize = compact.first()
+            if (i + matchSize > spread.length) continue // could be break?
+            val spreadEquivalent = spread.substring(i, i + matchSize)
+            val isMatch = spreadEquivalent.all { it != '.' }
+            val hasDelimiter = i + matchSize == spread.length || spread[i + matchSize] != '#'
+            if (isMatch) count += when {
+                i + matchSize == spread.length -> arrangements("", compact.drop(1), cache)
+                hasDelimiter -> {
+                    val next = spread.substring(i + matchSize + 1)
+                    val nextCompact = compact.drop(1)
+                    val key = "$next-${nextCompact.hashCode()}"
+                    cache.getOrPut(key) { arrangements(next, nextCompact, cache) }
+                }
+                else -> 0
+            }
+        }
+        return if (compact.isEmpty()) 1 else count
+    }
+    fun part2_2(input: List<String>): Long {
+        return input.sumOf {
+            val (conditionRecord, groupSizes) = it.split(' ')
+            val cr = ("$conditionRecord?").repeat(5).dropLast(1)
+            val groups = ("$groupSizes,").repeat(5).dropLast(1).split(',').map { it.toInt() }
+            arrangements(cr, groups)
+        }
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test")
     check(part1(testInput) == 21)
 
     val input = readInput("Day12")
     part1(input).println()
-    part2(input).println()
+    part2_2(input).println()
 }
