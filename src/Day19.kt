b37df535a180aed7287f8878d38730c25e6f8ad8
Christian Frommeyer <christian.frommeyer@personio.de>
2023-12-20 11:31:55 +0100

    Day19 part 2 done

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,7 +1,10 @@
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun buildWorkflows(input: List<String>): Map<String, Workflow> {
         val regex = """(?:([xmas])([<>])([0-9]+):)?([a-zAR]+),?""".toRegex()
-        val workflows = input.takeWhile { it.isNotBlank() }.map {
+        return input.takeWhile { it.isNotBlank() }.map {
             val matches = sequence {
                 var matcher = regex.find(it)
                 while(matcher != null) {
@@ -18,6 +21,10 @@ fun main() {
                 }
             })
         }.associateBy { it.name }
+    }
+
+    fun part1(input: List<String>): Int {
+        val workflows = buildWorkflows(input)
 
         return input.takeLastWhile { it.isNotBlank() }.mapNotNull {
             val valuePairs = it.substring(1, it.lastIndex).split(',')
@@ -32,8 +39,31 @@ fun main() {
         }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val workflows = buildWorkflows(input)
+        val paths = mutableListOf(Path("in", mapOf(
+            Pair('x', Triple('x', 1, 4000)),
+            Pair('m', Triple('m', 1, 4000)),
+            Pair('a', Triple('a', 1, 4000)),
+            Pair('s', Triple('s', 1, 4000)),
+        )))
+        var accepted = 0L
+        while(paths.isNotEmpty()) {
+            val path = paths.removeFirst()
+            when (path.target) {
+                "A" -> accepted += path.size()
+                "R" -> continue
+                else -> {
+                    val wf = workflows[path.target]!!
+                    var ranges = path.ranges
+                    for (rule in wf.rules) {
+                        paths.add(Path(rule.target, rule.applyRange(ranges, rule.range())))
+                        ranges = rule.applyRange(ranges, rule.reverseRange())
+                    }
+                }
+            }
+        }
+        return accepted
     }
 
     // test if implementation meets criteria from the description, like:
@@ -41,6 +71,9 @@ fun main() {
     val part1 = part1(testInput)
     if(part1 != 19114)
         throw IllegalStateException("Wrong test result: $part1")
+    val part2 = part2(testInput)
+    if(part2 != 167409079868000)
+        throw IllegalStateException("Wrong test result: $part2")
 
     val input = readInput("Day19")
     part1(input).println()
@@ -49,11 +82,35 @@ fun main() {
 
 abstract class WFRule(val variable: Char, val value: Int, val target: String) {
     abstract fun matches(part: Map<Char, Int>): Boolean
+
+    abstract fun range(): Triple<Char, Int, Int>
+
+    abstract fun reverseRange(): Triple<Char, Int, Int>
+
+    open fun applyRange(ranges: Map<Char, Triple<Char, Int, Int>>, range: Triple<Char, Int, Int>): Map<Char, Triple<Char, Int, Int>> {
+        return if (ranges[variable] == null) {
+            ranges.plus(Pair(variable, range()))
+        } else {
+            ranges.mapValues {
+                if (it.key == variable) {
+                    Triple(variable, max(range.second, it.value.second), min(range.third, it.value.third))
+                } else it.value
+            }
+        }
+    }
 }
 
 class LtRule(variable: Char, value: Int, target: String) : WFRule(variable, value, target) {
     override fun matches(part: Map<Char, Int>): Boolean {
         return part[variable]!! < value
+    }
+
+    override fun range(): Triple<Char, Int, Int> {
+        return Triple(variable, 1, value - 1)
+    }
+
+    override fun reverseRange(): Triple<Char, Int, Int> {
+        return Triple(variable, value, 4000)
     }
 }
 
@@ -61,11 +118,31 @@ class GtRule(variable: Char, value: Int, target: String) : WFRule(variable, valu
     override fun matches(part: Map<Char, Int>): Boolean {
         return part[variable]!! > value
     }
+
+    override fun range(): Triple<Char, Int, Int> {
+        return Triple(variable, value + 1, 4000)
+    }
+
+    override fun reverseRange(): Triple<Char, Int, Int> {
+        return Triple(variable, 1, value)
+    }
 }
 
 class FinalRule(target: String) : WFRule('X', 0, target) {
     override fun matches(part: Map<Char, Int>): Boolean {
         return true
+    }
+
+    override fun range(): Triple<Char, Int, Int> {
+        return Triple('X', 0, 0) // not used
+    }
+
+    override fun reverseRange(): Triple<Char, Int, Int> {
+        return Triple('X', 0, 0)
+    }
+
+    override fun applyRange(ranges: Map<Char, Triple<Char, Int, Int>>, range: Triple<Char, Int, Int>): Map<Char, Triple<Char, Int, Int>> {
+        return ranges
     }
 }
 
@@ -77,4 +154,13 @@ class Workflow(val name: String, val rules: List<WFRule>) {
         }
         throw IllegalStateException()
     }
+}
+
+class Path(val target: String, val ranges: Map<Char, Triple<Char, Int, Int>>){
+    fun size(): Long {
+        var size = 1L
+        ranges.forEach { (_, range) -> size *= (range.third - range.second + 1) }
+        return size
+    }
+
 }
